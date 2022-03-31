37e354478d9e8d0f42ae1f54c919e55db40d5568
OctagonDeluge <junterz675@gmail.com>
2022-03-31 17:01:57 +0400

    test

---
+++ src/main/java/com/example/application/repository/EventRepository.java
@@ -0,0 +1,15 @@
+package com.example.application.repository;
+
+import com.example.application.entity.Event;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface EventRepository extends JpaRepository<Event, Long> {
+
+    @Query("SELECT sn FROM Event sn WHERE sn.schedule.id = ?1")
+    List<Event> findByScheduleId(Long eventId);
+}
---
+++ src/main/java/com/example/application/repository/ScheduleRepository.java
@@ -0,0 +1,28 @@
+package com.example.application.repository;
+
+import com.example.application.entity.Event;
+import com.example.application.entity.Schedule;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.sql.Date;
+import java.util.List;
+
+/*
+* Onload get events
+* Onclick get shortnote
+* Onclick form add shortnote
+*
+*
+*  */
+@Repository
+public interface ScheduleRepository extends JpaRepository<Schedule, Long> {
+
+    @Query("SELECT s FROM Schedule s WHERE s.event_date = ?1")
+    Schedule findByDate(Date scheduleDate);
+
+    @Query("SELECT s FROM Schedule s WHERE s.event_date BETWEEN ?1 AND ?2")
+    List<Schedule> findAllByMonth(Date beginOfMonth, Date endOfMonth);
+
+}
