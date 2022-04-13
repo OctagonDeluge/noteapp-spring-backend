b5c9b319b343027af1724549e89e4cb19d9d6889
OctagonDeluge <junterz675@gmail.com>
2022-04-13 21:54:28 +0400

    final version

--- src/main/java/com/example/application/repository/NoteRepository.java
+++ src/main/java/com/example/application/repository/NoteRepository.java
@@ -2,9 +2,16 @@ package com.example.application.repository;
 
 import com.example.application.entity.Note;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface NoteRepository extends JpaRepository<Note, Long> {
+
+    @Override
+    @Query("SELECT n FROM Note n WHERE n.user.email = ?#{principal?.username}")
+    List<Note> findAll();
+
 }
--- src/main/java/com/example/application/repository/UserRepository.java
+++ src/main/java/com/example/application/repository/UserRepository.java
@@ -2,12 +2,17 @@ package com.example.application.repository;
 
 import com.example.application.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+
     User findByEmail(String email);
+
+    @Query("SELECT u FROM User u WHERE u.email = ?#{principal?.username}")
+    User findCurrentByEmail();
 
     Boolean existsByEmail(String email);
 }
